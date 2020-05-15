c38faa319fd2919d01789ab002d84beed5893497
2sheds <oleg@kurapov.com>
2020-05-15 11:29:46 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM alpine:3.11
+MAINTAINER Oleg Kurapov (@kurapov)
+
+ENV UID="1000"
+ENV GUID="1000"
+
+VOLUME /certs
+VOLUME /etc/letsencrypt
+VOLUME /var/lib/letsencrypt
+WORKDIR /scripts
+
+COPY ./letsencrypt-autorenew-docker/scripts/run_certbot.sh . 
+COPY ./certbot-hook/hook.sh .
+COPY dedynauth .dedynauth
+COPY entrypoint.sh .
+RUN chmod -R +x . && \
+    apk update && \
+	apk add --no-cache openssl curl bind-tools bash ca-certificates certbot && \
+	rm -rf /tmp/* /var/tmp/*
+
+
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+DEDYNAUTH=`pwd`/.dedynauth
+if [ -f ${DEDYNAUTH} ]; then
+    source ${DEDYNAUTH}
+fi
+
+if [ -z "$DEDYN_TOKEN" ] ; then
+    >&2 echo "Variable \$DEDYN_TOKEN not found. Please set DEDYN_TOKEN=(your dedyn.io token) to your dedyn.io access token in $DEDYNAUTH, e.g."
+    >&2 echo 
+    >&2 echo "DEDYN_TOKEN=d41d8cd98f00b204e9800998ecf8427e"
+    exit 2
+fi
+
+if [ -z "$DEDYN_NAME" ] ; then
+    >&2 echo "Variable \$DEDYN_NAME not found. Please set DEDYN_NAME=(your dedyn.io name) to your dedyn.io name in $DEDYNAUTH, e.g."
+    >&2 echo
+    >&2 echo "DEDYN_NAME=foobar.dedyn.io"
+    exit 3
+fi
+
+if ./run_certbot.sh 
+
+then
+  for domain in $DOMAINS; do
+	d=${domain%%,*}
+    openssl pkcs12 -export -out /certs/$d.pfx -inkey /certs/$d.key.pem -in /certs/$d.cert.pem -certfile /certs/$d.chain.pem -password pass:$PASSWORD
+	cat /certs/$d.fullchain.pem /certs/$d.key.pem > /certs/$d.pem
+  done
+  chown ${UID}:${GUID} /certs/*
+  chmod o-rw /certs/*
+  chmod g+r /certs/*
+fi
